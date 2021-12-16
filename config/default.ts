8ffd421e1547291c107af26d2da04f2247754f42
tszhim mak <tszhimmak@ps.asl.com.hk>
2021-12-16 17:36:06 +0800

    - init project;

---
+++ config/default.ts
@@ -0,0 +1,12 @@
+export default {
+  port: 1337,
+  dbUri:
+    'mongodb+srv://dbUser:dbUserPassword@cluster0.rpqqn.mongodb.net/SCOPE_FYP_DB?retryWrites=true&w=majority',
+  saltWorkFactor: 10,
+  accessTokenTtl: '15m',
+  refreshTokenTtl: '1y',
+  accessTokenPrivateKey: ``,
+  accessTokenPublicKey: ``,
+  refreshTokenPrivateKey: ``,
+  refreshTokenPublicKey: ``,
+};
